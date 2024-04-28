cb1a13e3a7f3f97986c00e1645bed9ebd63107e3
Vivek Shaurya <97908880+MRPERFECT0603@users.noreply.github.com>
2024-04-28 10:59:25 +0530

    Update server.js for netlify

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 // app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://TheSocialEdge.netlify.app",
   credentials: true,
 }));
 app.use(cookieParser());
